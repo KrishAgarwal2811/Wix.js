4fdaf330b3ab22945ec103f1ba33b61b68d4de44
Wixonic <wixonic@icloud.com>
2021-05-08 13:02:47 +0200

    Adding shortcuts and re-writing some functions
    Co-authored-by: Tapabrata2005 <40142618+Tapabrata2005@users.noreply.github.com>
    
    - Adding document shortcut :
      - WElement.byId()
      - WElement.byClass()
      - WElement.byTag()
      - WElement.body
      - WElement.head
    
    - And re-writing syntax of :
      - WElement.age()
      - WElement.copy()
      - WElement.isWElement()
    
    - Avoid errors with window.loaded
    - Adding WE shortcut
    - Adding the NodeName property of an WElement

--- Wix.js
+++ Wix.js
@@ -1,7 +1,7 @@
 addEventListener("DOMContentLoaded",() => {
-	if (loaded instanceof Function)
+	if (window.loaded instanceof Function)
 	{
-		loaded();
+		window.loaded();
 	}
 });
 
@@ -35,6 +35,7 @@ const WElement = class {
 		} else {
 			this.#Node = N;
 			this.#Creation = Date.now();
+			this.nodeName = this.#Node.nodeName.toLowerCase();
 		}
 	}
 	
@@ -83,17 +84,88 @@ const WElement = class {
 		this.#Node.style.display = "";
 	}
 	
-	static age = (WE) => {
-		if (WElement.isWElement(WE))
+	static age(e)
+	{
+		if (WElement.isWElement(e))
 		{
-			return WE.age;
+			return e.age;
 		} else {
 			throw new TypeError(`Argument 1 must be an instance of WElement at WElement.age().`);
 		}
 	}
 	
-	static copy = (WE) => {
-		if (WElement.isWElement(WE))
+	static get body()
+	{
+		return new WElement(document.body);
+	}
+	
+	static byClass(c)
+	{
+		const e = document.getElementsByClassName(c);
+		
+		if (e == null)
+		{
+			return null;
+		} else {
+			let out = [];
+			
+			for (let x = 0; x < e.length; ++x)
+			{
+				const we = new WElement(e[x]);
+				
+				if (out.length > 0)
+				{
+					out.push(we);
+				} else {
+					out = [we];
+				}
+			}
+			
+			return out;
+		}
+	}
+	
+	static byId(i)
+	{
+		const e = document.getElementById(i);
+		
+		if (e == null)
+		{
+			return null;
+		} else {
+			return new WElement(e);
+		}
+	}
+	
+	static byTag(t)
+	{
+		const e = document.getElementsByTagName(t);
+		
+		if (e == null)
+		{
+			return null;
+		} else {
+			let out = [];
+			
+			for (let x = 0; x < e.length; ++x)
+			{
+				const we = new WElement(e[x]);
+				
+				if (out.length > 0)
+				{
+					out.push(we);
+				} else {
+					out = [we];
+				}
+			}
+			
+			return out;
+		}
+	}
+	
+	static copy(e)
+	{
+		if (WElement.isWElement(e))
 		{
 			return WE.copy();
 		} else {
@@ -101,5 +173,15 @@ const WElement = class {
 		}
 	}
 	
-	static isWElement = (WE) => WE instanceof WElement
+	static get head()
+	{
+		return new WElement(document.head);
+	}
+	
+	static isWElement(e)
+	{
+		return e instanceof WElement;
+	}
 };
+
+const WE = WElement;
